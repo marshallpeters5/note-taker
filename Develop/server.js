4fcd1523814ec67da5422dc9a00955c53954c5b2
Marshall <marshall.peters01@gmail.com>
2023-05-28 21:58:09 -0400

    added server.js and added all necessary routes

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const apiRoutes = require('./routes/noteRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}!`));
